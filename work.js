c78ea231a273d1bf4446baea8798807fcd6645fb
salman1753 <salmannawaz574@gmail.com>
2025-02-04 01:36:36 +0500

    Add files via upload

---
+++ work.js
@@ -0,0 +1,8 @@
+function humburg(){
+    const navbar = document.querySelector(".downdrop")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".downdrop")
+    navbar.style.transform = "translateY(-500px)"
+}
